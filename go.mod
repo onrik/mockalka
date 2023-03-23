e7e5b8e5e31aa97d300d0be3224406f647873881
Andrey <and@rey.im>
2023-03-23 15:03:05 +0300

    Init.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/onrik/mockalka
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"log"
+	"os"
+	"reflect"
+	"strings"
+)
+
+func main() {
+	path := ""
+	if len(os.Args) > 1 {
+		path = os.Args[1]
+	}
+	files, err := parser.ParseDir(token.NewFileSet(), path, nil, parser.ParseComments)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	for _, f := range files {
+		ast.Inspect(f, inspectFile)
+	}
+}
+
+func inspectFile(node ast.Node) bool {
+	t, ok := node.(*ast.TypeSpec)
+	if !ok {
+		return true
+	}
+
+	i, ok := t.Type.(*ast.InterfaceType)
+	if !ok {
+		return true
+	}
+	out := []string{}
+	name := fmt.Sprintf("%sMock", t.Name.String())
+	out = append(out, fmt.Sprintf("type %s struct {", name), "\tmock.Mock", "}", "")
+
+	for _, f := range i.Methods.List {
+		if len(f.Names) < 1 {
+			continue
+		}
+		funcName := f.Names[0]
+
+		ft := f.Type.(*ast.FuncType)
+		called := []string{}
+		params := []string{}
+		args := []string{}
+		for i, p := range ft.Params.List {
+			argName := fmt.Sprintf("arg%d", i)
+			if len(p.Names) > 0 {
+				argName = p.Names[0].String()
+			}
+			params = append(params, fmt.Sprintf("%s %s", argName, getType(p.Type)))
+			called = append(called, argName)
+		}
+
+		returns := []string{}
+		for i, r := range ft.Results.List {
+			if len(r.Names) == 0 {
+				returns = append(returns, getType(r.Type))
+			} else {
+				returns = append(returns, fmt.Sprintf("%s %s", r.Names[0], getType(r.Type)))
+			}
+			args = append(args, fmt.Sprintf("args.Get(%d).(%s)", i, getType(r.Type)))
+		}
+
+		out = append(
+			out,
+			fmt.Sprintf("func (m *%s) %s(%s) %s {", name, funcName, strings.Join(params, ", "), formatReturns(returns)),
+			fmt.Sprintf("\targs := o.Called(%s)", strings.Join(called, ", ")),
+			fmt.Sprintf("\treturn %s", strings.Join(args, ", ")),
+			"}",
+			"",
+		)
+	}
+
+	fmt.Println()
+	fmt.Println(strings.Join(out, "\n"))
+	fmt.Println()
+	return true
+}
+
+func getType(e ast.Expr) string {
+	switch t := e.(type) {
+	case *ast.Ident:
+		return t.Name
+	case *ast.ArrayType:
+		return "[]" + getType(t.Elt)
+	case *ast.SelectorExpr:
+		return getType(t.X) + "." + getType(t.Sel)
+	case *ast.StarExpr:
+		return "*" + getType(t.X)
+	case *ast.MapType:
+		return fmt.Sprintf("map[%s]%s", getType(t.Key), getType(t.Value))
+	case *ast.InterfaceType:
+		return "interface{}"
+	default:
+		log.Println("Unsupported type:", reflect.TypeOf(t))
+		return ""
+	}
+}
+
+func formatReturns(returns []string) string {
+	s := strings.Join(returns, ", ")
+	if len(returns) <= 1 {
+		return s
+	}
+
+	return "(" + s + ")"
+}

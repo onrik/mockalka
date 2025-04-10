d623dab66b516699dc2f64f11bea5e69bdd2586c
Andrey <and@rey.im>
2025-04-10 15:17:18 +0300

    Fix for funcs with void returns.

--- main.go
+++ main.go
@@ -21,9 +21,31 @@ func main() {
 		log.Println(err)
 		return
 	}
+
+	if len(files) > 0 {
+		for _, f := range files {
+			ast.Inspect(f, getPackageName)
+			break
+		}
+	}
+
+	fmt.Println(`import (
+	"github.com/stretchr/testify/mock"
+)`)
+
 	for _, f := range files {
 		ast.Inspect(f, inspectFile)
 	}
+}
+
+func getPackageName(node ast.Node) bool {
+	packge, ok := node.(*ast.Package)
+	if ok {
+		fmt.Println("package", packge.Name, "\n")
+		return true
+	}
+
+	return false
 }
 
 func inspectFile(node ast.Node) bool {
@@ -49,38 +71,56 @@ func inspectFile(node ast.Node) bool {
 		ft := f.Type.(*ast.FuncType)
 		called := []string{}
 		params := []string{}
-		args := []string{}
+
 		for i, p := range ft.Params.List {
-			argName := fmt.Sprintf("arg%d", i)
-			if len(p.Names) > 0 {
-				argName = p.Names[0].String()
+			if len(p.Names) == 0 {
+				argName := fmt.Sprintf("arg%d", i)
+				params = append(params, fmt.Sprintf("%s %s", argName, getType(p.Type)))
+				called = append(called, argName)
 			}
-			params = append(params, fmt.Sprintf("%s %s", argName, getType(p.Type)))
-			called = append(called, argName)
+
+			for _, name := range p.Names {
+				argName := name.String()
+				params = append(params, fmt.Sprintf("%s %s", argName, getType(p.Type)))
+				called = append(called, argName)
+			}
 		}
 
+		args := []string{}
 		returns := []string{}
-		for i, r := range ft.Results.List {
-			if len(r.Names) == 0 {
-				returns = append(returns, getType(r.Type))
-			} else {
-				returns = append(returns, fmt.Sprintf("%s %s", r.Names[0], getType(r.Type)))
+		if ft.Results != nil {
+			for i, r := range ft.Results.List {
+				if len(r.Names) == 0 {
+					returns = append(returns, getType(r.Type))
+				} else {
+					returns = append(returns, fmt.Sprintf("%s %s", r.Names[0], getType(r.Type)))
+				}
+				t := getType(r.Type)
+				arg := fmt.Sprintf("args.Get(%d)", i)
+				if t == "error" {
+					arg = fmt.Sprintf("args.Error(%d)", i)
+				} else {
+					arg = fmt.Sprintf("%s.(%s)", arg, t)
+				}
+				args = append(args, arg)
 			}
-			t := getType(r.Type)
-			arg := fmt.Sprintf("args.Get(%d)", i)
-			if t == "error" {
-				arg = fmt.Sprintf("args.Error(%d)", i)
-			} else {
-				arg = fmt.Sprintf("%s.(%s)", arg, t)
-			}
-			args = append(args, arg)
 		}
 
 		out = append(
 			out,
-			fmt.Sprintf("func (m *%s) %s(%s) %s {", name, funcName, strings.Join(params, ", "), formatReturns(returns)),
-			fmt.Sprintf("\targs := m.Called(%s)", strings.Join(called, ", ")),
-			fmt.Sprintf("\treturn %s", strings.Join(args, ", ")),
+			fmt.Sprintf("func (m *%s) %s(%s)%s {", name, funcName, strings.Join(params, ", "), formatReturns(returns)),
+		)
+
+		if len(args) > 0 {
+			out = append(
+				out,
+				fmt.Sprintf("\targs := m.Called(%s)", strings.Join(called, ", ")),
+				fmt.Sprintf("\treturn %s", strings.Join(args, ", ")),
+			)
+		}
+
+		out = append(
+			out,
 			"}",
 			"",
 		)
@@ -115,8 +155,8 @@ func getType(e ast.Expr) string {
 func formatReturns(returns []string) string {
 	s := strings.Join(returns, ", ")
 	if len(returns) <= 1 {
-		return s
+		return " " + s
 	}
 
-	return "(" + s + ")"
+	return " (" + s + ")"
 }
